63ce59e7585f9aa6fc2d778b4d8acc978e2eb6ae
Sebastjan O <sebaomahen@gmail.com>
2020-05-08 10:58:06 +0200

    Updates

---
+++ precache-manifest.62654193365f879a9ea0174d33691251.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66335a056004eb68e34",
+    "url": "/register/static/css/main.73588e86.chunk.css"
+  },
+  {
+    "revision": "b66335a056004eb68e34",
+    "url": "/register/static/js/main.b66335a0.chunk.js"
+  },
+  {
+    "revision": "ee5c47b2ece1f2456f38",
+    "url": "/register/static/css/1.76df734f.chunk.css"
+  },
+  {
+    "revision": "ee5c47b2ece1f2456f38",
+    "url": "/register/static/js/1.ee5c47b2.chunk.js"
+  },
+  {
+    "revision": "5a3d63a637ecfc550461",
+    "url": "/register/static/js/runtime~main.5a3d63a6.js"
+  },
+  {
+    "revision": "54d1eeebf5756eefdf14bad2d72e6fda",
+    "url": "/register/static/media/0.54d1eeeb.jpg"
+  },
+  {
+    "revision": "38de4360cb4cadf69880cdea055a37c6",
+    "url": "/register/static/media/shards-dashboards-logo.38de4360.svg"
+  },
+  {
+    "revision": "84a54819f5922d5a1f57b0b22ec7c971",
+    "url": "/register/index.html"
+  }
+];
